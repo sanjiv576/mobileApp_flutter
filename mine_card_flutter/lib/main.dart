477182aaa9b52aedd3548d88f60d63499e42af2d
sanjiv576 <sanjiv576>
2023-03-06 13:48:50 +0545

    update

--- mine_card_flutter/lib/main.dart
+++ mine_card_flutter/lib/main.dart
@@ -1,24 +1,50 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
 
-// 'stless' for boilerplate
+// // 'stless' for boilerplate
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.pink,
+//         body: SafeArea(
+//           child: Container(
+//             color: Colors.white,
+//             height: 100.0,
+//             width: 100.0,
+//             padding: const EdgeInsets.all(10.0),
+//             margin: const EdgeInsets.fromLTRB(10.4, 40.5, 5.0, 10.0),
+//             child: const Text('My Home'),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.pink,
+        backgroundColor: Colors.teal[200],
         body: SafeArea(
           child: Container(
+            height: 200,
+            width: 300,
             color: Colors.white,
-            height: 100.0,
-            width: 100.0,
-            padding: const EdgeInsets.all(10.0),
-            margin: const EdgeInsets.fromLTRB(10.4, 40.5, 5.0, 10.0),
-            child: const Text('My Home'),
+            alignment: Alignment.center,
+           // decoration: const BoxDecoration(shape: BoxShape.rectangle, color: Colors.blue,),
+            margin: const EdgeInsets.only(left: 50.75),
+            padding: const EdgeInsets.all(30),
+            child: const Text('Hello World'),
+
           ),
         ),
       ),
