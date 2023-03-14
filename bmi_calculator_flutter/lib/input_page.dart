1a9a9836173a1c41bb3cae62f39249ba16120339
sanjiv576 <sanjiv576>
2023-03-14 15:36:22 +0545

    add gender card color selection

--- bmi_calculator_flutter/lib/input_page.dart
+++ bmi_calculator_flutter/lib/input_page.dart
@@ -1,4 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
+import 'reusable_container_card.dart';
+
+const bottomContainerHeight = 80.0;
+
+const Color activeCardColour = Color(0xFF1D1E33);
+const Color bottomContainerColour = Color(0xFFFEB1555);
+const Color inactiveCardColour = Color(0xFF111328);
+  GenderType? selectedGender;
+
+// using enum for more readable instead of 0 , 1  for female and male
+// used enum when you have many options
+enum GenderType { male, female }
 
 class InputPage extends StatefulWidget {
   //const InputPage({super.key});
@@ -8,6 +22,11 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  // Color maleColour = inactiveCardColour;
+  // Color femaleColour = inactiveCardColour;
+  
+  // old code for gender selection , method is in the end
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,51 +38,118 @@ class _InputPageState extends State<InputPage> {
         ),
       ),
       body: Column(children: <Widget>[
-        
         Expanded(
-            
             child: Row(
-            
           children: <Widget>[
             Expanded(
-              child: ReusableContainerCard(colour: Color(0xFF1D1E33)),
+              child: GestureDetector(
+                onTap: () {
+                  // print("Male icon is tapped");
+                  setState(() {
+                    // old technique without using Ternary operator
+                    // updateColour(gender: GenderType.male);
+
+                    selectedGender = GenderType.male;
+                  });
+                },
+                child: ReusableContainerCard(
+                   // Using Ternary Operation
+
+                  colour: selectedGender == GenderType.male
+                        ? activeCardColour
+                        : inactiveCardColour,
+                  cardChild: IconContent(
+                      genderIcon: FontAwesomeIcons.mars, gendertType: "MALE"),
+                ),
+              ),
             ),
             Expanded(
-              child: ReusableContainerCard(colour: Color(0xFF1D1E33)),
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    // old technique without using Ternary operator
+                    // updateColour(gender: GenderType.female);
+
+                    selectedGender = GenderType.female;
+                  });
+                },
+                child: ReusableContainerCard(
+                  colour: selectedGender == GenderType.female
+                        ? activeCardColour
+                        : inactiveCardColour,
+                  cardChild: IconContent(
+                    genderIcon: FontAwesomeIcons.venus,
+                    gendertType: "FEMALE",
+                  ),
+                ),
+              ),
             ),
           ],
         )),
-         Expanded(child: ReusableContainerCard(colour: Color(0xFF1D1E33)),),
         Expanded(
-            
+          child: ReusableContainerCard(colour: activeCardColour),
+        ),
+        Expanded(
             child: Row(
-            
           children: <Widget>[
             Expanded(
-              child: ReusableContainerCard(colour: Color(0xFF1D1E33)),
+              child: ReusableContainerCard(colour: activeCardColour),
             ),
             Expanded(
-              child: ReusableContainerCard(colour: Color(0xFF1D1E33)),
+              child: ReusableContainerCard(colour: activeCardColour),
             ),
           ],
         )),
+        Container(
+          // takes all width for all mobile devices
+          width: double.infinity,
+          height: bottomContainerHeight,
+          margin: EdgeInsets.only(top: 10.0),
+          color: bottomContainerColour,
+        )
       ]),
     );
   }
 }
 
-class ReusableContainerCard extends StatelessWidget {
-  // const ReusableContainerCard({super.key});
+//  custom Widget was here i.e IconContent
 
-  Color colour;
-  ReusableContainerCard({required this.colour});
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8.0),
-           color: colour),
-    );
-  }
-}
+//  custom Widget was here i.e ReusableContainerCard
+
+
+// old code
+
+// 0 - female, 1 - male
+//   void updateColour({required GenderType gender}) {
+//     // activate male and deactivate female background colour, if male is tapped
+//     if (gender == GenderType.male) {
+//       // if (maleColour == inactiveCardColour) {
+//       //   maleColour = activeCardColour;
+//       //   femaleColour = inactiveCardColour;
+//       // } else {
+//       //   maleColour = inactiveCardColour;
+//       // }
+
+// // used Ternary Operator to reduce above comment code
+//       maleColour = maleColour != inactiveCardColour
+//           ? inactiveCardColour
+//           : activeCardColour;
+//       femaleColour = inactiveCardColour;
+//     }
+//     // activate female & deactivate male background colour, if female is tapped
+//     else {
+//       // if (femaleColour == inactiveCardColour) {
+//       //   femaleColour = activeCardColour;
+//       //   maleColour = inactiveCardColour;
+//       // } else {
+//       //   femaleColour = inactiveCardColour;
+//       // }
+
+// // used Ternary Operator to reduce above comment code
+
+//       femaleColour = femaleColour == activeCardColour
+//           ? inactiveCardColour
+//           : activeCardColour;
+//       maleColour = inactiveCardColour;
+//     }
+//   }
