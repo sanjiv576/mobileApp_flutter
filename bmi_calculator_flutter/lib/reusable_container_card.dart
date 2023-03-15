48e06c19c979e7cece8761f9654518b355461bb1
sanjiv576 <sanjiv576>
2023-03-15 16:21:30 +0545

    add GestureDetector after removing from input_page

--- bmi_calculator_flutter/lib/reusable_container_card.dart
+++ bmi_calculator_flutter/lib/reusable_container_card.dart
@@ -5,14 +5,21 @@ class ReusableContainerCard extends StatelessWidget {
 
   final Color colour;
   final Widget? cardChild;
-  ReusableContainerCard({required this.colour, this.cardChild});
+  // function for higher order function
+  //  Note: VoidCallback is just shorthand for void Function()
+  final VoidCallback? customOnPress;
+  ReusableContainerCard({required this.colour, this.cardChild, this.customOnPress});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8.0), color: colour),
-      child: cardChild,
+    return GestureDetector(
+      // passing the customOnPress function
+      onTap: customOnPress,
+      child: Container(
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.0), color: colour),
+        child: cardChild,
+      ),
     );
   }
 }
