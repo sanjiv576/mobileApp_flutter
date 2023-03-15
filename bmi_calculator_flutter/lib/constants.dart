cf9e91e417be71dbc9bba91775ced3c009e8f68e
sanjiv576 <sanjiv576>
2023-03-15 16:19:54 +0545

    separate constant properties

---
+++ bmi_calculator_flutter/lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+// This dart file contains only constant properties
+
+// used in input_page dart file --> Note: constant variable name starts with 'k', just a standard convention
+const kBottomContainerHeight = 80.0;
+const Color activeCardColour = Color(0xFF1D1E33);
+const Color kBottomContainerColour = Color(0xFFFEB1555);
+const Color inactiveCardColour = Color(0xFF111328);
+
+const kValueLableTextStyle =
+    TextStyle(fontSize: 50.0, fontWeight: FontWeight.w900);
+
+// used in icon_content file
+const kLabelTextStyle = TextStyle(color: Color(0xFF8D8E98), fontSize: 18.0);
