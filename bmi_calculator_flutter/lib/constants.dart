12c6bda28a090d6faba9a0e411a6a17be6af5f82
sanjiv576 <sanjiv576>
2023-03-16 16:54:57 +0545

    add constant properties for result_page

--- bmi_calculator_flutter/lib/constants.dart
+++ bmi_calculator_flutter/lib/constants.dart
@@ -10,5 +10,20 @@ const Color inactiveCardColour = Color(0xFF111328);
 const kValueLableTextStyle =
     TextStyle(fontSize: 50.0, fontWeight: FontWeight.w900);
 
+const kLargeButtonTextStyle =
+    TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold);
+
 // used in icon_content file
 const kLabelTextStyle = TextStyle(color: Color(0xFF8D8E98), fontSize: 18.0);
+
+// used in results_page file
+
+const kTitleTextStyle = TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold);
+
+const KHealthStatusTextStyle = TextStyle(fontSize: 22.0, color: Color(0xFF34D876), fontWeight: FontWeight.bold);
+
+const kCalculatedValueTextStyle = TextStyle(fontSize: 100.0, fontWeight: FontWeight.bold);
+
+const kMessageTextStyle = TextStyle(fontSize: 22.0, color: Colors.white);
+
+ 
