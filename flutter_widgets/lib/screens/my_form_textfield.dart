778cdc40e700d152db424f6e304daae7dbca7d02
sanjiv576 <sanjiv576>
2023-03-19 15:34:23 +0545

    inital commit

---
+++ flutter_widgets/lib/screens/my_form_textfield.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyTextFormField extends StatelessWidget {
+ // const MyTextFormField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Text Form Field'),
+        backgroundColor: Colors.purple,
+      ),
+      body: Center(),
+    );
+  }
+}
