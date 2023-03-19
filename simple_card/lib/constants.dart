6126b01ba29c2751be7388498a633718ac2c38b4
sanjiv576 <sanjiv576>
2023-03-19 10:50:21 +0545

    separate from main.dart constant properties

---
+++ simple_card/lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// contains constant properties only
+
+const kBackGroundColor = Color.fromRGBO(8, 9, 8, 0.494);
+const kLabelTextStyle = TextStyle(color: Colors.white);
+const kTitleTextStyle = TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold);
+const kIconTitleTextStyle =
+    TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.black);
+const kDescriptionTextStyle = TextStyle(
+    fontSize: 12.0, fontWeight: FontWeight.normal, color: Colors.black);
+const kButtomIconTextStyle = TextStyle(
+  fontSize: 30.0,
+);
