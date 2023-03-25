526c15b49c698556be85bac240b69e1177a4ed84
sanjiv576 <shresthasanjiv576@gmail.com>
2023-03-25 14:39:37 +0545

    add methods for getting weather data by city names or geographical data

--- clima_flutter/lib/services/weather.dart
+++ clima_flutter/lib/services/weather.dart
@@ -1,4 +1,44 @@
+import '../services/location.dart';
+import '../services/networking.dart';
+import '../api_key.dart';
+
 class WeatherModel {
+  double? latitude;
+  double? longitude;
+
+  Future<dynamic> getCityWeather({required String cityName}) async {
+    // url for getting weather by city
+    String url =
+        'https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey&units=metric';
+
+    return sendWeatherData(url: url);
+  }
+
+  Future<dynamic> sendWeatherData({required String url}) async {
+    NetworkHelper networkHelper = NetworkHelper(url: url);
+
+    var weatherData = await networkHelper.getData();
+    // print(weatherData);
+
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    latitude = location.latitude;
+    longitude = location.longitude;
+    // print('Latitude : $latitude');
+    // print('Longitude: $longitude');
+
+// units=metric --> converts fahrenheit into celsius
+    String url =
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric';
+
+    return sendWeatherData(url: url);
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
