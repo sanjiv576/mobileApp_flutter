554d61e28844aa864e1910ab2d3f1bb292f2fe65
sanjiv576 <shresthasanjiv576@gmail.com>
2023-03-25 14:33:32 +0545

    add async methods to fetch data from open weather org

--- clima_flutter/lib/screens/loading_screen.dart
+++ clima_flutter/lib/screens/loading_screen.dart
@@ -1,8 +1,8 @@
+import 'package:clima_flutter/services/weather.dart';
 import 'package:flutter/material.dart';
-import '../services/location.dart';
-import 'package:http/http.dart' as http;
-
 import '../api_key.dart';
+import '../screens/location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -10,52 +10,44 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  late Location location;
-
-  void getLocation() async {
-    await location.getCurrentLocation();
-
-    print('Latitude : ${location.latitude}');
-    print('Longitude: ${location.longitude}');
-  }
+//  late Location location;
 
 // immediately this method is called when the screen opens or LooadingScreen object is created
   @override
   void initState() {
     super.initState();
 
-    location = Location();
-    getLocation();
+    // location = Location();
+    getLocationData();
   }
 
 // immediately this method is called when the screen is closed or popped
-
   @override
   void deactivate() {
     super.deactivate();
     print('Deactivating stateful widget');
   }
 
-  void getData() async {
-    String url =
-        'https://api.openweathermap.org/data/2.5/weather?q=Kathmandu&appid=$apiKey';
-    http.Response response = await http.get(Uri.parse(url));
-    print(response.body);
+  void getLocationData() async {
+    WeatherModel weatherModel = WeatherModel();
+    var weatherData = await weatherModel.getLocationWeather();
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => LocationScreen(
+                  locationWeather: weatherData,
+                )));
   }
 
   @override
   Widget build(BuildContext context) {
-    getData();
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            //Get the current location
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
+          child: SpinKitFadingCircle(
+        color: Colors.white,
+        size: 80.0,
+      )),
     );
   }
 }
