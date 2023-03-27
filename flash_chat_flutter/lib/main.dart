b5cac79648be0332896731769ac85003f4b68e0a
sanjiv576 <shresthasanjiv576@gmail.com>
2023-03-27 11:56:10 +0545

    initial commit

---
+++ flash_chat_flutter/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import '../screens/welcome_screen.dart';
+import '../screens/login_screen.dart';
+import '../screens/registration_screen.dart';
+import '../screens/chat_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.black54),
+        ),
+      ),
+      // home: WelcomeScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => WelcomeScreen(),
+        '/login': (context) => LoginScreen(),
+        '/register': (context) => RegistrationScreen(),
+        '/chat': (context) => ChatScreen()
+      },
+    );
+  }
+}
