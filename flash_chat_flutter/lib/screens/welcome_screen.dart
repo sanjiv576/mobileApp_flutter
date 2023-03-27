0ab8dabdd8d66291f987a6bbe5f49d2db0f7e29e
sanjiv576 <shresthasanjiv576@gmail.com>
2023-03-27 14:59:37 +0545

    add linear and non-linear animations

--- flash_chat_flutter/lib/screens/welcome_screen.dart
+++ flash_chat_flutter/lib/screens/welcome_screen.dart
@@ -1,15 +1,86 @@
+import 'package:flash_chat_flutter/screens/login_screen.dart';
+import 'package:flash_chat_flutter/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatefulWidget {
+  static const String id = 'welcome_screen';
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+// SingleTickerProviderStateMixin is used as a ticker
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  // manager that manages/controls animations duration, start, stop, move forward or reverse
+  AnimationController? controller;
+
+  // another animation for curve animation
+  Animation? animation;
+
+// we need animation when the screen appears, so, using inside the initState()
+  @override
+  void initState() {
+    super.initState();
+
+// for linear animation
+    controller = AnimationController(
+      // how long the animation should complete ?
+      duration: Duration(seconds: 3),
+      // vsync works as a ticker --> ticker means like a ticking clock that counts the animation as the shape, color, size changes
+      vsync: this,
+
+      // Note: upperBound is 1 by default, and never while using in CurvedAnimation
+      // it throws error because by default CurvedAnimaiton has 1.0 upperBound value not others
+      // upperBound: 100,
+    );
+
+    // for non-linear animaiton
+    animation = CurvedAnimation(parent: controller!, curve: Curves.decelerate);
+
+    // move the animation forward
+    controller!
+        .forward(); // --> its status = AnimaitonStatus.completed (when it is completed)
+
+    // loop animation
+
+    // controller!.addStatusListener((status) {
+    //   //  print(status);
+    //   // animate reverse when the forward() is completed
+    //   if (status == AnimationStatus.completed) {
+    //     controller!.reverse(from: 1.0);
+    //   }
+    //   // animate forwared when the reverse() is dismissed
+    //   else if (status == AnimationStatus.dismissed) {
+    //     controller!.forward();
+    //   }
+    // });
+
+    // for Tween animation
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
+        .animate(controller!);
+
+    // observe the generating value between 0 and 1 by AnimationController,
+    // for this we should listen it
+    controller!.addListener(() {
+      setState(() {});
+      // print(controller!.value);
+      print(animation!.value);
+    });
+  }
+
+  // always dispose the animation for free space
+
+  @override
+  void dispose() {
+    controller!.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      // backgroundColor: Colors.red.withOpacity(controller!.value),
+      backgroundColor: animation!.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -18,9 +89,14 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
+                // add Hero animation --> starting point
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    // height: animation!.value * 100,
+                    height: 60.0,
+                  ),
                 ),
                 Text(
                   'Flash Chat',
@@ -43,6 +119,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 child: MaterialButton(
                   onPressed: () {
                     //Go to login screen.
+                    Navigator.pushNamed(context, LoginScreen.id);
                   },
                   minWidth: 200.0,
                   height: 42.0,
@@ -61,6 +138,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 child: MaterialButton(
                   onPressed: () {
                     //Go to registration screen.
+                    Navigator.pushNamed(context, RegistrationScreen.id);
                   },
                   minWidth: 200.0,
                   height: 42.0,
