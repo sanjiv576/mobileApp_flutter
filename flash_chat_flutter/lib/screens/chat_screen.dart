c58c6e5206c8a00affc3e03d69d854e7e8ff084e
sanjiv576 <shresthasanjiv576@gmail.com>
2023-03-29 16:04:23 +0545

    fetch data from the cloud automatically

--- flash_chat_flutter/lib/screens/chat_screen.dart
+++ flash_chat_flutter/lib/screens/chat_screen.dart
@@ -38,6 +38,28 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
+// method -1 to get data from the cloud, but, we need to pull data from the cloud manually
+  void getMessages() async {
+    // get data from the cloud in the form of QuerySnapshot<Map> so loop it to get each message
+    final messages = await _firestore.collection('messages').get();
+
+    for (var message in messages.docs) {
+      // get each message
+      print(message.data());
+    }
+  }
+
+  // Method -2 to get data from the cloud, automatically fetch data when the changes occur
+  void messageStream() async {
+    // get data in Stream when the changes occur in the cloud
+    await for (var snapshot in _firestore.collection('messages').snapshots()) {
+      // fetch data in the form of Map
+      for (var message in snapshot.docs) {
+        print(message.data());
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,10 +69,14 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: Icon(Icons.logout),
               onPressed: () {
+                // Example 1
+                //  getMessages();
+                //  Example 2
+                messageStream();
                 //Implement logout functionality
-                _auth.signOut();
-                print('Logged out');
-                Navigator.pop(context);
+                // _auth.signOut();
+                // print('Logged out');
+                // Navigator.pop(context);
               }),
         ],
         title: Text('⚡️Chat'),
@@ -82,13 +108,14 @@ class _ChatScreenState extends State<ChatScreen> {
                           'Sender : ${loggedInUser!.email} \n Message : $messageText');
                       // send data to the cloud firebase
                       try {
+                        // Note: messages --> is the collection name in the cloud,
+                        // text and sender are the keys that store values in Map ie key and value pairs
                         _firestore.collection('messages').add({
                           'text': messageText,
                           'sender': loggedInUser!.email
                         });
                       } catch (e) {
                         print(e);
-                        
                       }
                     },
                     child: Text(
