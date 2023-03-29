03029e6c118bbd8a717f7ccbfe82eeda3d2afc44
sanjiv576 <shresthasanjiv576@gmail.com>
2023-03-29 15:26:29 +0545

    add firebase and send data to the cloud

--- flash_chat_flutter/lib/screens/chat_screen.dart
+++ flash_chat_flutter/lib/screens/chat_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:math';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import '../constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   static const String id = 'chat_screen';
@@ -9,6 +14,30 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final _auth = FirebaseAuth.instance;
+  User? loggedInUser;
+  final _firestore = FirebaseFirestore.instance;
+  String? messageText;
+
+  @override
+  void initState() {
+    super.initState();
+
+    getCurrentUser();
+  }
+
+  void getCurrentUser() {
+    try {
+      final user = _auth.currentUser;
+      if (user != null) {
+        loggedInUser = user;
+        print('Active users: $loggedInUser');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,9 +45,12 @@ class _ChatScreenState extends State<ChatScreen> {
         leading: null,
         actions: <Widget>[
           IconButton(
-              icon: Icon(Icons.close),
+              icon: Icon(Icons.logout),
               onPressed: () {
                 //Implement logout functionality
+                _auth.signOut();
+                print('Logged out');
+                Navigator.pop(context);
               }),
         ],
         title: Text('⚡️Chat'),
@@ -38,6 +70,7 @@ class _ChatScreenState extends State<ChatScreen> {
                     child: TextField(
                       onChanged: (value) {
                         //Do something with the user input.
+                        messageText = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
@@ -45,6 +78,18 @@ class _ChatScreenState extends State<ChatScreen> {
                   ElevatedButton(
                     onPressed: () {
                       //Implement send functionality.
+                      print(
+                          'Sender : ${loggedInUser!.email} \n Message : $messageText');
+                      // send data to the cloud firebase
+                      try {
+                        _firestore.collection('messages').add({
+                          'text': messageText,
+                          'sender': loggedInUser!.email
+                        });
+                      } catch (e) {
+                        print(e);
+                        
+                      }
                     },
                     child: Text(
                       'Send',
