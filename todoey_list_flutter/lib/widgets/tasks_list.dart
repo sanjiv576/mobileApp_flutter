255521845b316e4b3d91f2b1c8f1c00747aabffc
sanjiv576 <shresthasanjiv576@gmail.com>
2023-04-02 19:27:04 +0545

    add tasks list

--- todoey_list_flutter/lib/widgets/tasks_list.dart
+++ todoey_list_flutter/lib/widgets/tasks_list.dart
@@ -1,29 +1,45 @@
 import 'package:flutter/material.dart';
 import '../constants.dart';
 import 'tasks_tile.dart';
+import '../models/tasks.dart';
+import '../screens/tasks_screen.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  List<Tasks>? tasks;
+  TasksList({required this.tasks});
+
+
+  @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  // List<Tasks> tasks = [
+  //   Tasks(taskName: 'Revise HTML'),
+  //   Tasks(taskName: 'Develop group chat app')
+  // ];
+
+  //TasksScreen tasksScreen = TasksScreen();
+
   @override
   Widget build(BuildContext context) {
-    return ListView(children: <Widget>[
-      TasksTile(),
-      TasksTile(),
-      TasksTile(),
-      TasksTile(),
-      TasksTile(),
-      TasksTile(),
-      TasksTile(),
-      TasksTile(),
-      TasksTile(),
-      TasksTile(),
-      TasksTile(),
-      TasksTile(),
-      TasksTile(),
-      TasksTile(),
-      TasksTile(),
-      TasksTile(),
-      TasksTile(),
-      TasksTile(),
-    ]);
+    // creates dynamic listview
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        // returns each data from tasks list
+        return TasksTile(
+          isChecked: widget.tasks![index].isDone,
+          taskTitle: widget.tasks![index].taskName,
+          toggleCheckboxState: (bool currentChechboxState) {
+            setState(() {
+              // toggle both states of text and checkbox from UI as true/false
+              widget.tasks![index].toggleDone();
+              // isChecked = currentChechboxState;
+            });
+          },
+        );
+      },
+      itemCount: widget.tasks!.length,
+    );
   }
 }
