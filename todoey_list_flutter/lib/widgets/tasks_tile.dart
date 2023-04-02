147bc5199ca8925f389a51f3ecd84c54d1ab15b9
sanjiv576 <shresthasanjiv576@gmail.com>
2023-04-02 19:27:17 +0545

    update UI

--- todoey_list_flutter/lib/widgets/tasks_tile.dart
+++ todoey_list_flutter/lib/widgets/tasks_tile.dart
@@ -1,49 +1,49 @@
 import 'package:flutter/material.dart';
 import '../constants.dart';
 
-class TasksTile extends StatefulWidget {
-  @override
-  State<TasksTile> createState() => _TasksTileState();
-}
+class TasksTile extends StatelessWidget {
+  bool? isChecked;
 
-class _TasksTileState extends State<TasksTile> {
-  bool isChecked = false;
+  final String? taskTitle;
+
+  final toggleCheckboxState;
+
+  TasksTile({required this.isChecked, required this.taskTitle, required this.toggleCheckboxState});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-        title: Text(
-          'Eat 1 food',
-          style: TextStyle(
-              decoration: isChecked ? TextDecoration.lineThrough : null),
-        ),
-        trailing: TaskCheckBox(
-          checkboxState: isChecked,
-          toggleCheckboxState: (bool currentChechboxState) {
-            setState(() {
-              // toggle both states of text and checkbox from UI as true/false
-              isChecked = currentChechboxState;
-            });
-          },
-        ));
-  }
-}
-
-class TaskCheckBox extends StatelessWidget {
-  // const TaskCheckBox({super.key});
-
-  final bool? checkboxState;
-  final toggleCheckboxState;
-
-  const TaskCheckBox(
-      {required this.checkboxState, required this.toggleCheckboxState});
-
-  @override
-  Widget build(BuildContext context) {
-    return Checkbox(
-      value: checkboxState,
-      activeColor: Colors.lightBlueAccent,
-      onChanged: toggleCheckboxState,
+      title: Text(
+        taskTitle.toString(),
+        style: TextStyle(
+          fontSize: 18.0, fontWeight: FontWeight.bold ,
+            decoration: isChecked! ? TextDecoration.lineThrough : null),
+      ),
+      trailing: Checkbox(
+        value: isChecked,
+        activeColor: Colors.lightBlueAccent,
+        onChanged: toggleCheckboxState,
+      ),
     );
   }
 }
+
+
+// class TaskCheckBox extends StatelessWidget {
+//   // const TaskCheckBox({super.key});
+
+//   final bool? checkboxState;
+//   final toggleCheckboxState;
+
+//   const TaskCheckBox(
+//       {required this.checkboxState, required this.toggleCheckboxState});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Checkbox(
+//       value: checkboxState,
+//       activeColor: Colors.lightBlueAccent,
+//       onChanged: toggleCheckboxState,
+//     );
+//   }
+// }
